4963fe433a2387be109bdbee9df8037280989637
Matt Krafft <mattkrafft@gmail.com>
2026-08-07 12:16:53 -0500

    Name and reposition US bases

---
+++ base-config.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Named U.S. interceptor bases and normalized map positions.
+defenseBases[0].x=.10;
+defenseBases[0].y=.47;
+defenseBases[0].name="AL UDEID";
+
+defenseBases[1].x=.37;
+defenseBases[1].y=.65;
+defenseBases[1].name="AL DHAFRA";
